16ca8a8ce5a9dd67d88bfd60f6a09e0841bd6c8f
alexandra deniaud <alexandra.deniaud@telecom-paris.fr>
2020-10-15 17:54:54 +0200

    Updates

---
+++ precache-manifest.243a27b2c0d292551b5f48a963991456.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac2721431aa71b5a787c5a0a396febe",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "4e0348a77715e72893b0",
+    "url": "/blindtest-spotify/static/css/main.12217c1b.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0348a77715e72893b0",
+    "url": "/blindtest-spotify/static/js/main.3d0c9d89.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
